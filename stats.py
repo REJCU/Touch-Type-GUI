0616d3dc2db0e0bba1b60edd7545e621ebc42033
REJCU <ryan.ericson@my.jcu.edu.au>
2026-02-06 15:13:50 +1000

    Prints out the date and records it on the json file

--- stats.py
+++ stats.py
@@ -3,6 +3,9 @@ Takes results of touch typing session and saves it to a json file.
 """
 
 import json 
+import datetime
+
+attempt_date = datetime.datetime.now()
 
 class RecordResults():
     def __init__(self):
@@ -11,9 +14,14 @@ class RecordResults():
     def print_to_json(self, data):
         # Sends to the json file. 
         with open("results.json", "a") as f:
+            date =  f"{attempt_date.day}-{attempt_date.month}-{attempt_date.year}"
+            # Init the new assignment
+            data["Date"] = date
             data = json.dumps(data)
             print(data)
-            f.write(data)
+            print(type(data))
+            print(date)
+            f.write(data + "\n")
 
 
 
